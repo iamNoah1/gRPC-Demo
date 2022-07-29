80f8ee8dac39bf066b552a300178a2d4c6709265
Noah Ispas (iamNoah1) <noahispas@gmail.com>
2022-07-29 19:39:06 +0200

    simplify server stream

--- README.md
+++ README.md
@@ -5,10 +5,24 @@
 * grpcurl installed (or any other tool to make grpc "requests")
 
 ## Run 
-* `dotnet run`
 
-## Endpoints
+As there are different scenarios when using grpc, there are separate projects that can be run. 
+
+### Unary 
+
+Client sends request, server sends response
+
+* `dotnet run --project Server`
 * `grpcurl -plaintext -d '{"name": "noah"}' localhost:5226 Greeter/UnaryCall`
+
+### Server Stream
+
+Clients sends request, server streams responses
+
+* `dotnet run --project Server`
+* `dotnet run --project TempClient`
 
 ## Additional resources
 * [grpcurl](https://github.com/fullstorydev/grpcurl)
+* https://github.com/ckadluba/GrpcDemos 
+* https://github.com/stevejgordon/gRPC-Demos
--- TempClient/Program.cs
+++ TempClient/Program.cs
@@ -6,12 +6,11 @@ using Server;
 using var channel = GrpcChannel.ForAddress("http://localhost:5226");
 var client = new TemperatureReceiver.TemperatureReceiverClient(channel);
 
-var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-using var streamingCall = client.GetTemperatureStream(new Empty(), cancellationToken: cts.Token);
+using var streamingCall = client.GetTemperatureStream(new Empty());
 
 try
 {
-    await foreach (var weatherData in streamingCall.ResponseStream.ReadAllAsync(cancellationToken: cts.Token))
+    await foreach (var weatherData in streamingCall.ResponseStream.ReadAllAsync())
     {
         Console.WriteLine($"{weatherData.DateTimeStamp.ToDateTime():s} | {weatherData.TemperatureC} C");
     }
